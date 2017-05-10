b0ef968f384886f3f0ca40a8f49c8e6da6590050
Jack Burns <jack@raptiq.com>
2017-05-10 02:59:58 -0400

    add hash conversion

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -4,15 +4,24 @@ defmodule Identicon do
   """
 
   @doc """
-  Hello world.
-
+    Given a string deterministic create an identicon
+    pipes output through helpers until image is created
   ## Examples
 
-      iex> Identicon.hello
+      iex> Identicon.main
       :world
 
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @doc """
+  Generate a list of numbers from a string
+  """
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
