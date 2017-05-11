e8f2a26fe936decc30163dc010e0773d704d9086
Jack Burns <jack@raptiq.com>
2017-05-11 03:30:36 -0400

    add image struct w/ color and grid pipelines

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -15,13 +15,38 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+    |> build_grid
   end
 
   @doc """
   Generate a list of numbers from a string
   """
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+    %Identicon.Image{hex: hex}
   end
+
+  @doc """
+  Extract image color from first three hex values
+  """
+  def pick_color(image) do
+    [r, g, b | _tail] = image.hex
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+  @doc """
+
+  """
+  def build_grid(image) do
+    image.hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
 end
